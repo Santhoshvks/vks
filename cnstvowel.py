870dd8cf257e10ba34ba9246234cab38617e4a2f
Santhoshvks <36471883+Santhoshvks@users.noreply.github.com>
2018-02-15 11:53:51 +0530

    Update and rename alpha.py to cnstvowel.py

---
+++ cnstvowel.py
@@ -0,0 +1,14 @@
+i = input("Input a letter of alphabet: ")
+
+
+if i in ('a', 'e', 'i', 'o', 'u'):
+	
+print("%s is a vowel." % i)
+
+elif i == 'y':
+	
+print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+
+else:
+	print("%s is a consonant." % i) 
+	
