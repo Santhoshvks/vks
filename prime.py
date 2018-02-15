a8174277134e69d0dd1505725ab7ed8cfab0005d
Santhoshvks <36471883+Santhoshvks@users.noreply.github.com>
2018-02-15 15:23:52 +0530

    Update and rename ppr.py to prime.py

--- prime.py
+++ prime.py
@@ -1,9 +1,9 @@
 a=int(input("Enter number: "))
-k=0
+num=0
 for i in range(2,a//2+1):
     if(a%i==0):
-        k=k+1
-if(k<=0):
+        num=num+1
+if(num<=0):
     print("Number is prime")
 else:
     print("Number isn't prime")
