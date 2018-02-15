5d5092a528644c80d01c1450abde860941b8a2b9
Santhoshvks <36471883+Santhoshvks@users.noreply.github.com>
2018-02-15 11:19:30 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,14 +1,14 @@
-l = input("Input a letter of the alphabet: ")
+s = input("Input a letter of alphabet: ")
 
 
-if l in ('a', 'e', 'i', 'o', 'u'):
+if s in ('a', 'e', 'i', 'o', 'u'):
 	
-print("%s is a vowel." % l)
+print("%s is a vowel." % s)
 
-elif l == 'y':
+elif s == 'y':
 	
 print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
 
 else:
-	print("%s is a consonant." % l) 
+	print("%s is a consonant." % s) 
 	
