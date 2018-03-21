6d6b127d5c1531d62ecd76fc16f6ad405f6e1ee9
Santhoshvks <36471883+Santhoshvks@users.noreply.github.com>
2018-03-21 17:36:11 +0530

    Update and rename amstrng.py to amstrongno.py

--- amstrongno.py
+++ amstrongno.py
@@ -1,11 +1,11 @@
-i = int(input("Enter a number: "))
+num= int(input("Enter a number: "))
 sum = 0
-temp = i
+temp = num
 while temp > 0:
    digit = temp % 10
    sum += digit ** 3
    temp //= 10
-if i == sum:
+if num == sum:
    print(num,"is an Armstrong number")
 else:
    print(num,"is not an Armstrong number")
