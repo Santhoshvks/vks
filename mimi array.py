b13af9bace2d66dbe326ff17281221421a0fa60f
Santhoshvks <36471883+Santhoshvks@users.noreply.github.com>
2018-05-24 09:54:12 +0530

    Update and rename min array.py to mimi array.py

--- mimi array.py
+++ mimi array.py
@@ -1,8 +1,8 @@
-lst = []
+list = []
 num = int(input('How many numbers: '))
  
 for n in range(num):
     numbers = int(input('Enter number '))
-    lst.append(numbers)
+    list.append(numbers)
  
-print("Minimum element in the list is :", min(lst))
+print("Minimum element in the list is :", min(list))
